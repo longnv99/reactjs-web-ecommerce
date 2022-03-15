22fff60b2385033d0be1800837c379a5a5e2caf3
longnv99 <litlong99@gmail.com>
2022-03-15 17:12:22 +0700

    fix data slide

--- src/assets/data/data-slider.js
+++ src/assets/data/data-slider.js
@@ -14,14 +14,14 @@ const sliderData = [
         title: "Áo Polo cổ khóa",
         description: "Nếu bạn là người có thân hình cân đối với chiều cao và cân nặng lý tưởng thì đây là kiểu áo thời thượng mà bạn không nên bỏ qua cho mình trong quá trình sử dụng.",
         img: img2,
-        path: "/catalog/polo-shirt-02",
+        path: "/catalog/t-shirt-04",
         color: "black"
     },
     {
         title: "Áo sơ mi trắng",
         description: "Chiếc áo sơ mi giờ đây đã vượt lên hẳn ý nghĩa của một trang phục thông thường để trở thành nét văn hóa của con người hiện đại. Đó là văn hóa của hội nhập, của hiện đại và sự thanh lịch. Giờ đây, sơ mi cũng là văn hóa.",
         img: img3,
-        path: "/catalog/t-shirt-03",
+        path: "/catalog/shirt-03",
         color: "white"
     }
 ]
